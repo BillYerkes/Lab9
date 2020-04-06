3279bb698f34fb34a36a860ac825170303ef8b49
BillYerkes <43595904+BillYerkes@users.noreply.github.com>
2020-04-06 18:54:44 -0500

    Initial push
    init

---
+++ test.bat
@@ -0,0 +1,11 @@
+curl -H "Authorization: jwt eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJib2R5Ijoic3R1ZmYiLCJpYXQiOjE1ODYxOTQ0Mjd9.dm1cuawnnZVin7DIhHeK0DVg8CaERrfHgPwjWX9S8XQ" http://localhost:3000/secret
+
+curl http://localhost:3000/readme
+
+curl http://localhost:3000/
+
+curl -H "Authorization: jwt " http://localhost:3000/secret
+
+curl http://localhost:3000/secret
+
+curl -H "Authorization: jwt JhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJib2R5Ijoic3R1ZmYiLCJpYXQiOjE1ODYxOTQ0Mjd9.dm1cuawnnZVin7DIhHeK0DVg8CaERrfHgPwjWX9S8XQ" http://localhost:3000/secret
